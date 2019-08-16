707be88497600a1d728453052bd9c439d95dc066
C7ement <34455787+C7ement@users.noreply.github.com>
2019-08-16 17:42:49 +0100

    Add files via upload

--- tutorial-network/lib/logic.js
+++ tutorial-network/lib/logic.js
@@ -4,8 +4,20 @@
  * @transaction
  */
 async function sampleTransaction(schedUpdate) {  // eslint-disable-line no-unused-vars
-    schedUpdate.schedule.value = schedUpdate.newValue;
+    let newSchedule = schedUpdate.newValue;
+    let oldSchedule = schedUpdate.schedule.value;
+
+    let differenceArray = oldSchedule.map( (s, t) => {
+	return Math.abs(newSchedule[t]-oldSchedule[t])
+	});
+    let difference = differenceArray.reduce((a,b)=>a+b) / differenceArray.length;
+
+    schedUpdate.schedule.value = newSchedule;
+    schedUpdate.schedule.diffrenceWithPreviousSchedule = difference;
+    schedUpdate.schedule.s_endOfDay = schedUpdate.s_endOfDay;
+
     let assetRegistry = await getAssetRegistry('org.example.biznet.Schedule');
     await assetRegistry.update(schedUpdate.schedule);
 }
+
 
